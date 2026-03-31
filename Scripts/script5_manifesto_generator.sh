a0a670dc059175e14d42d40d0f7f74d8015f81f8
Shivansh1445-code <Shivanshpandey1442005@gmail.com>
2026-03-31 19:29:00 +0530

    Update script5_manifesto_generator.sh

--- Scripts/script5_manifesto_generator.sh
+++ Scripts/script5_manifesto_generator.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Script 5: Open Source Manifesto Generator
-# Author: Havya Kalpeshkumar Thakar
+# Author: Shivansh Pandey
 
 echo "Answer three questions to generate your manifesto."
 echo ""
